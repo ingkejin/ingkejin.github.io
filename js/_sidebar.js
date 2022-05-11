b192feba759e4b3e507c2f01eadf26975dda6db9
yejin <ingkejin@gmail.com>
2022-05-11 22:10:42 +0900

    🐛 Fixed Javascript SyntaxError

--- js/_sidebar.js
+++ js/_sidebar.js
@@ -1,6 +1,6 @@
 const sidebarMenuButton = document.querySelector('.gnb-icon-button.is-menu');
-const sidebar = document.querySelector('.sidebar')
-const sidebarOverlay = document.querySelector('.overlay')
+const sidebar = document.querySelector('.sidebar');
+const sidebarOverlay = document.querySelector('.overlay');
 
 function openSidebar() {
   sidebar.classList.add('is-active');
